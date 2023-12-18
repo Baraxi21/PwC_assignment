a1926718ebba45904bbb5d162c67266956592a7a
Baraxi21 <150504802+Baraxi21@users.noreply.github.com>
2023-12-19 01:21:40 +0400

    Add files via upload

---
+++ quiz_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from quiz_generator import generate_quiz_question_with_options
+
+st.title('MCQ Quiz')
+
+def reset_quiz_state():
+    st.session_state.quiz_data = {}
+    st.session_state.generated = False
+    st.session_state.score = 0
+
+if 'quiz_data' not in st.session_state:
+    reset_quiz_state()
+
+api_key = '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' 
+ 
+topic = st.text_input("Enter a topic for your quiz:", key="new_topic")
+num_questions = st.number_input("How many questions do you want?", min_value=1, max_value=10, value=10, key="num_questions")
+
+if st.button('Generate Quiz'):
+    reset_quiz_state()
+    st.session_state.generated = True
+    for i in range(1, num_questions + 1):
+        question, options, correct_answer = generate_quiz_question_with_options(topic, api_key)
+        st.session_state.quiz_data[f"question_{i}"] = {"question": question, "options": options, "correct_answer": correct_answer}
+
+if st.session_state.generated:
+    for i in range(1, num_questions + 1):
+        data = st.session_state.quiz_data[f"question_{i}"]
+        st.write(f"Question {i}: {data['question']}")
+        chosen_option = st.radio("Select your answer:", data['options'], key=f"answer_{i}_{st.session_state.new_topic}_{st.session_state.num_questions}")
+
+if st.session_state.generated and st.button('Submit Answers'):
+    score = 0
+    for i in range(1, num_questions + 1):
+        user_answer = st.session_state[f"answer_{i}_{st.session_state.new_topic}_{st.session_state.num_questions}"]
+        correct_answer = st.session_state.quiz_data[f"question_{i}"]["correct_answer"]
+        if user_answer == correct_answer:
+            score += 1
+    st.write(f"Your final score is {score}/{num_questions}")
+    reset_quiz_state() 
+
+if st.button('Start New Quiz'):
+    if 'quiz_data' in st.session_state:
+        del st.session_state['quiz_data']
---
+++ quiz_generator.py
@@ -0,0 +1,30 @@
+from langchain.llms import OpenAI
+
+def generate_quiz_question_with_options(topic, api_key):
+    llm = OpenAI(api_key='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')
+
+    prompt = f"Create a multiple choice quiz question about {topic}, along with 4 options (A, B, C, D) and indicate the correct answer."
+    response = llm(prompt, max_tokens=150, temperature=0.7).strip()
+    
+    return parse_response(response)
+
+def parse_response(response):
+    
+    lines = response.split('\n')
+    question = lines[0].split(": ", 1)[1]
+
+    options = []
+    for line in lines[1:5]:
+        option_parts = line.split(') ', 2) if ') ' in line else line.split('. ', 2)
+        if len(option_parts) > 1:
+            options.append(option_parts[1].strip())
+        else:
+            options.append("Option not found")
+    correct_answer_letter = lines[-1].split(": ")[1].strip()
+    if correct_answer_letter:
+        correct_answer_letter = correct_answer_letter[0]
+
+    correct_option_index = ord(correct_answer_letter.upper()) - ord('A')  
+    correct_option = options[correct_option_index] if 0 <= correct_option_index < len(options) else None
+
+    return question, options, correct_option
